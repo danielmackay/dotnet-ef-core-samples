44e35c0751b2f7288700eb51d86f2a0c1a3fdb0c
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-04 21:09:24 +1000

    Added compile queries

---
+++ CompiledQueries/ProductQueries.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompiledQueries;
+
+internal class ProductQueries
+{
+    public static readonly Func<ApplicationDbContext, string, Product?> GetFirstOrDefault =
+        EF.CompileQuery((ApplicationDbContext dbContext, string name) =>
+            dbContext.Set<Product>().FirstOrDefault(p => p.Name == name));
+
+    public static readonly Func<ApplicationDbContext, List<Product>> GetAll =
+        EF.CompileQuery((ApplicationDbContext dbContext) =>
+                dbContext.Set<Product>().ToList());
+}
---
+++ CompiledQueries/Program.cs
@@ -0,0 +1,33 @@
+﻿using CompiledQueries;
+
+Console.WriteLine("Compiled Queries Sample");
+
+using var db = new ApplicationDbContext();
+await db.Database.EnsureDeletedAsync();
+await db.Database.EnsureCreatedAsync();
+
+var products = new List<Product>()
+{
+    new Product() { Name = "Product 1" },
+    new Product() { Name = "Product 2" },
+    new Product() { Name = "Product 3" },
+    new Product() { Name = "Product 4" },
+    new Product() { Name = "Product 5" },
+    new Product() { Name = "Product 6", IsDeleted = true },
+    new Product() { Name = "Product 7", IsDeleted = true },
+    new Product() { Name = "Product 8", IsDeleted = true },
+    new Product() { Name = "Product 9" , IsDeleted = true},
+    new Product() { Name = "Product 10" , IsDeleted = true}
+};
+
+await db.Products.AddRangeAsync(products);
+await db.SaveChangesAsync();
+Console.WriteLine("Products added");
+
+var product = ProductQueries.GetFirstOrDefault(db, "Product 1");
+Console.WriteLine($"Product {product.Name}");
+
+var allProducts = ProductQueries.GetAll(db);
+allProducts.ForEach(p => Console.WriteLine($"Product {p.Name}"));
+
+Console.ReadLine();
