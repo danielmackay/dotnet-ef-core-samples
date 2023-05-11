b82c7b3b9d403d80d3e03b64dbe53d5e3b3339ed
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-11 17:08:48 +1000

    Added TPH

---
+++ TablePerHierarchy/Complex/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<FarmAnimal> FarmAnimals { get; set; }
+
+    public DbSet<Pet> Pets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=ComplexTph;Trusted_Connection=True");
+
+        optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
---
+++ TablePerHierarchy/Program.cs
@@ -0,0 +1,9 @@
+﻿using TablePerHierarchy.Simple;
+
+Console.WriteLine("Table Per Hierarchy Sample");
+
+using var simpleDb = new SimpleDbContext();
+await simpleDb.Database.EnsureDeletedAsync();
+await simpleDb.Database.EnsureCreatedAsync();
+
+Console.ReadLine();
