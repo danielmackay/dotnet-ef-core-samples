b84bb6e2f38973f11c4e78f321e1124e6d3a8ce2
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-11 17:52:50 +1000

    Added TPT

---
+++ TablePerType/Simple/Entities.cs
@@ -0,0 +1,15 @@
+﻿namespace TablePerType.Simple;
+
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Species { get; set; } = null!;
+}
+
+public class Pet : Animal
+{
+    public string? FavoriteToy { get; set; }
+    public string? Vet { get; set; }
+}
---
+++ TablePerType/Simple/SimpleDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace TablePerType.Simple;
+
+public class SimpleDbContext : DbContext
+{
+    public DbSet<Animal> Animals { get; set; }
+
+    public DbSet<Pet> Pets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=SimpleTpt;Trusted_Connection=True");
+
+        optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Animal>().UseTptMappingStrategy();
+    }
+}
