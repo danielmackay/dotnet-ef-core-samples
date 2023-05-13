00c0e089b61dedbdefbeba68eb11d589414e25d5
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-14 07:39:06 +1000

    ✨39 sequences (#46)
    * Added sequences
    
    * Tidy up

---
+++ Sequences/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Sequences;Trusted_Connection=True");
+
+        //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasSequence<int>("OrderNumbers")
+            .StartsAt(100)
+            .IncrementsBy(10);
+
+        modelBuilder
+            .Entity<Order>()
+            .Property(o => o.OrderNumber)
+            .HasDefaultValueSql("NEXT VALUE FOR OrderNumbers");
+    }
+}
---
+++ Sequences/Entities.cs
@@ -0,0 +1,10 @@
+﻿public class Order
+{
+    public int Id { get; set; }
+    public int OrderNumber { get; set; }
+
+    override public string ToString()
+    {
+        return $"Id: {Id}, OrderNumber: {OrderNumber}";
+    }
+}
