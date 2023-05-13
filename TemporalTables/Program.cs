4b0c9710b5b95f8153c29f469de4bdfbf128ff48
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-13 10:40:47 +1000

    ✨ Added temporal tables (#38)
    Added temporal tables

---
+++ TemporalTables/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Temporal Tables Sample");
+
+using var db = new ApplicationDbContext();
+db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+var product = new Product() { Name = "Product 1", Price = 10.00M };
+
+db.Products.Add(product);
+db.SaveChanges();
+var originalTs = DateTime.UtcNow;
+
+Thread.Sleep(2000);
+
+product.Price = 15.00M;
+db.SaveChanges();
+var middleTs = DateTime.UtcNow;
+
+Thread.Sleep(2000);
+
+product.Price = 20.00M;
+db.SaveChanges();
+var latestTs = DateTime.UtcNow;
+
+Console.WriteLine("Original Product");
+product = db.Products.TemporalAsOf(originalTs).First();
+PrintTimestamp(originalTs);
+PrintProduct(product);
+
+Console.WriteLine("Middle Product");
+product = db.Products.TemporalAsOf(middleTs).First();
+PrintTimestamp(middleTs);
+PrintProduct(product);
+
+Console.WriteLine("Latest Product");
+product = db.Products.TemporalAsOf(latestTs).First();
+PrintTimestamp(latestTs);
+PrintProduct(product);
+
+var temporalAll = db.Products.TemporalAll().ToList();
+temporalAll.ForEach(p => PrintProduct(p));
+
+Console.ReadLine();
+
+static void PrintProduct(Product product) => 
+    Console.WriteLine($"Product - ID:{product.Id}, Name:{product.Name}, Price:{product.Price}");
+
+static void PrintTimestamp(DateTime timestamp) =>
+    Console.WriteLine($"Timestamp - {timestamp:HH:mm:ss}");
