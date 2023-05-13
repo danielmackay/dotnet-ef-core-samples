784964004e916f0ed097aae217ef73e230107c18
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-14 07:37:38 +1000

    Added sequences

---
+++ Sequences/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Owned Entities Sample");
+
+using var db = new ApplicationDbContext();
+db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+var orders = new List<Order>()
+{
+    new Order(),
+    new Order(),
+    new Order()
+};
+
+db.Orders.AddRange(orders);
+db.SaveChanges();
+
+var allOrders = db.Orders.ToList();
+allOrders.ForEach(Console.WriteLine);
+
+Console.ReadLine();
+
---
+++ Sequences/readme.md
@@ -0,0 +1,11 @@
+# Sequences
+
+Sequences allow you to generate sequential values for a column in a database. This is useful for generating unique identifiers for entities in a database in a fashion that is not sequential.
+
+## Use Cases
+
+- Order numbers
+
+## Resources
+
+- https://learn.microsoft.com/en-us/ef/core/modeling/sequences
