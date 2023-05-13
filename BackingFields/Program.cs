1dafef930fe530b15f4c7ec6888e2ac0f6098a3e
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-13 11:17:10 +1000

    ✨ 28 Backing fields (#42)
    * Added backing fields
    
    * Added project back to solution

---
+++ BackingFields/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Backing Fields Sample");
+
+using var db = new ApplicationDbContext();
+db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+var product = new Product
+{
+    Name = "Product 1",
+    Price = 10.00M,
+    VoucherCode = "abc"
+};
+
+db.Products.Add(product);
+db.SaveChanges();
+
+product = db.Products.First();
+PrintProduct(product);
+
+Console.ReadLine();
+
+static void PrintProduct(Product product) =>
+    Console.WriteLine($"Product - ID:{product.Id}, Name:{product.Name}, Price:{product.Price}");
---
+++ BackingFields/readme.md
@@ -0,0 +1,8 @@
+# Backing Fields
+
+Backing fields let's you bind a field to a database column.  
+
+## Use Cases
+
+- Encapsulate access to a private fields
+- Read only private fields that you want to persist
