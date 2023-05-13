12003042787d1aa905e2bf35df50de91662c1f78
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-14 07:03:08 +1000

    Added value converter

---
+++ ValueConverters/Product.cs
@@ -0,0 +1,8 @@
+﻿public class Product
+{
+    public required ProductId Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public record ProductId(Guid Value);
