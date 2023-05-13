ec530b4502039d273605659c8066e12017cda990
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-13 10:38:51 +1000

    Added temporal tables

---
+++ TemporalTables/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=TemporalTables;Trusted_Connection=True");
+
+        //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Product>()
+            .ToTable("Products", b => b.IsTemporal());
+    }
+}
---
+++ TemporalTables/Product.cs
@@ -0,0 +1,6 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+}
