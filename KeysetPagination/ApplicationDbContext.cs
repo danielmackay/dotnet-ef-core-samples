9aa78092adb55f278391a320cd840e6a64195bce
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-14 16:36:59 +1000

    ✨ Added keyset pagination (#55)
    Added keyset pagination

---
+++ KeysetPagination/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=KeysetPagination;Trusted_Connection=True");
+
+        //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
---
+++ KeysetPagination/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Keyset Pagination Sample");
+
+using var db = new ApplicationDbContext();
+db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+var products = Enumerable
+    .Range(1, 100)
+    .Select(i => new Product { Name = $"Product {i}", Price = i * 10.00M });
+
+db.Products.AddRange(products);
+db.SaveChanges();
+
+const int pageSize = 10;
+var currentPage = 0;
+
+Console.WriteLine("Offset-based Pagination");
+bool hasItems = false;
+do
+{
+    var productPage = db.Products
+        .OrderBy(p => p.Id)
+        .Skip(pageSize * currentPage)
+        .Take(pageSize)
+        .ToList();
+
+    productPage.ForEach(Console.WriteLine);
+    hasItems = productPage.Count > 0;
+    currentPage++;
+
+} while (hasItems);
+
+Console.WriteLine("Keyset-based Pagination");
+hasItems = false;
+int lastId = 0;
+do
+{
+    var productPage = db.Products
+        .OrderBy(p => p.Id)
+        .Where(p => p.Id > lastId)
+        .Take(pageSize)
+        .ToList();
+
+    productPage.ForEach(Console.WriteLine);
+    lastId = productPage.LastOrDefault()?.Id ?? 0;
+    hasItems = productPage.Count > 0;
+
+} while (hasItems && lastId != 0);
+
+Console.ReadLine();
