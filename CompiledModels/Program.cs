f93d0f7a6a280a659bcff5d6177bf3607f3e5fdb
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-05-17 07:33:43 +1000

    Added compiled models

---
+++ CompiledModels/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Compiled Models Sample");
+
+using var db = new ApplicationDbContext();
+db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+var products = Enumerable
+    .Range(1, 10)
+    .Select(i => new Product { Name = $"Product {i}", Price = i * 10.00M });
+
+db.Products.AddRange(products);
+db.SaveChanges();
+
+var allProducts = db.Products.ToList();
+allProducts.ForEach(Console.WriteLine);
+
+Console.ReadLine();
