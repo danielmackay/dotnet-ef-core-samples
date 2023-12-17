189a95c0e581eb7b9da9de3e48e611075d332236
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-12-17 10:00:42 +1000

    Add Common.csproj, DbConnectionFactory.cs, and docker-compose.yml

---
+++ Common/DbConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Common;
+
+public static class DbConnectionFactory
+{
+    public static string Create(string databaseName)
+    {
+        var builder = new SqlConnectionStringBuilder
+        {
+            DataSource = "localhost:1433",
+            InitialCatalog = databaseName,
+            IntegratedSecurity = true,
+            MultipleActiveResultSets = true
+        };
+
+        return builder.ConnectionString;
+    }
+}
