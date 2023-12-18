2cd924317f3c6083b43ae9e89ee4dcc797747749
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-12-18 22:24:44 +1000

    ✨ Added complex types (#73)
    Added complex types

---
+++ ComplexTypes/Contact.cs
@@ -0,0 +1,25 @@
+﻿namespace ComplexTypes;
+
+public class Contact
+{
+    public int Id { get; init; }
+    public required string Name { get; init; }
+    public required Address Address { get; init; }
+
+    public override string ToString()
+    {
+        return $"ID:{Id}, Name:{Name}, Address:{Address}";
+    }
+}
+
+public class Address
+{
+    public required string Street { get; init; }
+    public required string City { get; init; }
+    public required string PostCode { get; init; }
+
+    public override string ToString()
+    {
+        return $"Street:{Street}, City:{City}, PostCode:{PostCode}";
+    }
+}
