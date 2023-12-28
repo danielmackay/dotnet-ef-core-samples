c9d3c01bbeb6976611a436fe489712c98ac07b60
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-12-28 11:01:00 +1000

    Added SentinelValues.csproj

---
+++ SentinelValues/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnhancedJsonColumns;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(DbConnectionFactory.Create("EnhancedJsonColumns"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().OwnsMany(e => e.Colors, builder => builder.ToJson());
+    }
+}
---
+++ SentinelValues/Program.cs
@@ -0,0 +1,55 @@
+﻿using EnhancedJsonColumns;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Enhanced JSON Columns Sample");
+
+using var db = new ApplicationDbContext();
+db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+var products = new List<Product>
+{
+    new()
+    {
+        Name = "Product 1",
+        Colors =
+        [
+            new Color { Name = "Black", Code = ColorCode.Black, NumInStock = 10 },
+            new Color { Name = "White", Code = ColorCode.White, NumInStock = 5 },
+            new Color { Name = "Red", Code = ColorCode.Red, NumInStock = 2 },
+        ]
+    },
+    new()
+    {
+        Name = "Product 2",
+        Colors =
+        [
+            new Color { Name = "Blue", Code = ColorCode.Blue, NumInStock = 10 },
+            new Color { Name = "Brown", Code = ColorCode.Brown, NumInStock = 5 },
+            new Color { Name = "Green", Code = ColorCode.Green, NumInStock = 2 },
+        ]
+    },
+    new()
+    {
+        Name = "Product 3",
+        Colors =
+        [
+            new Color { Name = "Black", Code = ColorCode.Black, NumInStock = 10 },
+            new Color { Name = "Purple", Code = ColorCode.Purple, NumInStock = 5 },
+            new Color { Name = "Yellow", Code = ColorCode.Yellow, NumInStock = 2 },
+        ]
+    },
+};
+
+db.Products.AddRange(products);
+db.SaveChanges();
+
+var blackProducts = db.Products
+    .AsNoTracking()
+    .SelectMany(p => p.Colors.Where(c => c.Code == ColorCode.Black))
+    .ToList();
+
+Console.WriteLine("Black Products");
+blackProducts.ForEach(Console.WriteLine);
+
+Console.ReadLine();
